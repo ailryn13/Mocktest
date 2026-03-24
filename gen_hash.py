466a7a6387c3730e383f3b5662a1f55036019fe5
Your Name <you@example.com>
2026-03-24 23:16:19 +0530

    Clean workspace and finalize Judge0 fixes

--- gen_hash.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import bcrypt
-h = bcrypt.hashpw(b'student123', bcrypt.gensalt(10)).decode()
-print(h)
-
-with open('/tmp/update_pwd.sql', 'w') as f:
-    f.write("UPDATE users SET password_hash='" + h + "' WHERE email='dhinesh2004@gmail.com';\n")
-print("SQL written to /tmp/update_pwd.sql")
--- test_judge0.sh
+++
@@ -1,6 +0,0 @@
-curl -s -X POST "http://judge0-server:2358/submissions?base64_encoded=false&wait=true" \
--H "Content-Type: application/json" \
--d '{
-  "source_code": "public class Main {\n  public static void main(String[] args) {\n    System.out.print(\"Hello world\");\n  }\n}",
-  "language_id": 62
-}'
